8cbea22fe010b1b0a401e77e85ad4ba864cf6998
SashaGriga <a.grigoryev@polytechncm.com>
2021-06-21 17:05:36 +0300

    Initial commit

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,41 +1,45 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
-
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
+    UserDao dao = new UserDaoHibernateImpl();
+
+
     @Override
     public void createUsersTable() {
-        (new UserDaoJDBCImpl()).createUsersTable();
+        dao.createUsersTable();
     }
 
     @Override
     public void dropUsersTable() {
-        (new UserDaoJDBCImpl()).dropUsersTable();
+        dao.dropUsersTable();
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        (new UserDaoJDBCImpl()).saveUser(name, lastName, age);
+        dao.saveUser(name, lastName, age);
     }
 
     @Override
     public void removeUserById(long id) {
-        (new UserDaoJDBCImpl()).removeUserById(id);
+        dao.removeUserById(id);
     }
 
     @Override
     public List<User> getAllUsers() {
-        return (new UserDaoJDBCImpl()).getAllUsers();
+        return dao.getAllUsers();
     }
 
     @Override
     public void cleanUsersTable() {
-        (new UserDaoJDBCImpl()).cleanUsersTable();
+        dao.cleanUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,24 +1,57 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
-    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String URL = "jdbc:mysql://localhost:3306/mydbtest?serverTimezone=UTC";
-    private static final String USER = "root";
-    private static final String PASSWORD = "root";
+	private static final String URL = "jdbc:mysql://localhost:3306/mydbtest?serverTimezone=UTC&useSSL=false";
+	private static final String USERNAME = "root";
+	private static final String PASSWORD = "root";
 
-    public Connection getConnection() {
-        Connection connection = null;
-        try {
-            Class.forName(DRIVER);
-            connection = DriverManager.getConnection(URL, USER, PASSWORD);
-        } catch (ClassNotFoundException | SQLException e) {
-            System.out.println("Соединение с БД не установлено");
-            e.printStackTrace();
-        }
-        return connection;
-    }
+	public static Connection getConnection() {
+		Connection connection = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		} catch (ClassNotFoundException | SQLException e) {
+			System.out.println("Соединение не установлено");
+			e.printStackTrace();
+		}
+		return connection;
+	}
+
+	private static SessionFactory sessionFactory;
+	public static SessionFactory getSessionFactory() {
+		if (sessionFactory == null) {
+			try {
+				Configuration configuration = new Configuration();
+
+				Properties properties = new Properties();
+				properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+				properties.put(Environment.URL, URL);
+				properties.put(Environment.USER, USERNAME);
+				properties.put(Environment.PASS, PASSWORD);
+				properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+
+				configuration.setProperties(properties);
+				configuration.addAnnotatedClass(User.class);
+
+				ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+						.applySettings(configuration.getProperties()).build();
+
+				sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return sessionFactory;
+	}
 }
